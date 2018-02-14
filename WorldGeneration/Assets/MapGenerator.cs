408c8b164b4eec10b9fd06cfc4d434fa304f12ae
Hylus <Hylus@wp.pl>
2018-02-14 19:27:47 +0100

    Perlin noise
    Create a grid of perlin noise values and render them to the screen as a texture

---
+++ WorldGeneration/Assets/MapGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public bool autoUpdate; // only use to CustomEditor - MapGeneratedEditor
+
+    [SerializeField] int _mapWidth;
+    [SerializeField] int _mapHeigh;
+    [SerializeField] float _noiseScale;
+    
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(_mapWidth, _mapHeigh, _noiseScale);
+
+        MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
+        mapDisplay.DrawNoiseMap(noiseMap);   
+    }
+
+}
